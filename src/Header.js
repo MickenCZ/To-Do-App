2664e6d7b6175d8ed8592a455e4a56ef6acc8d0c
MickenCZ <MickenCZseznam.cz>
2021-08-17 11:27:50 +0200

    Initial files

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+const stylesHeader = {
+  textAlign:"center",
+  color:"#EEEEEE",
+  backgroundColor:"#5B45B9",
+  fontSize:"25px",
+  width:"100%",
+  paddingTop:"1.25rem",
+  paddingBottom:"1.25rem",
+  borderRadius:"1rem 1rem 0rem 0rem",
+  fontWeight:300,
+}
+
+function Header() {
+  return <header style={stylesHeader}>
+  TO-DO LIST
+  </header>
+}
+
+export default Header
---
+++ src/Todos.js
@@ -0,0 +1,17 @@
+import "./Todos.css"
+
+function Todos() {
+  return <div>
+    <section>
+
+    </section>
+
+
+    <footer id="footer">
+    <input placeholder="Add item" id="footerInput" />
+    <button id="footerButton">ADD ITEM</button>
+    </footer>
+  </div>
+}
+/*maybe add onclick animation to button like in bootstrap, also make it responsive */
+export default Todos
