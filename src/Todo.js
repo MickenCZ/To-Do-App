563404d74622c272701e4cef8a16cdecd0bcc017
MickenCZ <MickenCZseznam.cz>
2021-08-18 11:14:11 +0200

    Added remove feature

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,21 @@
 import "./Todo.css"
 
+function Todo(props) {
+
   function removeTodo() {
-    //change localstorage, then use props.setTodos to change state.
+    //Removes the item with the id of this component from localstorage, then updates state.
+    let todosArray = JSON.parse(window.localStorage.getItem("todos"))
+    for (let i = 0; i < todosArray.length; i++) {
+      if (todosArray[i].id === props.id) {
+        todosArray.splice(i, 1)
+        window.localStorage.setItem("todos", JSON.stringify(todosArray))
+        props.setTodos(JSON.parse(window.localStorage.getItem("todos")))
+        break
+      }
+      
+    }
   }
 
-function Todo(props) {
   return <div id="todoWrapper">
           <span id="todoText">{props.text}</span>
             <div id="buttons">
